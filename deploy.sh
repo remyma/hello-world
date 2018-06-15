1713cffd30db78afa7011ff8c35e12ebae7e7406
Matthieu REMY <remy.matthieu@gmail.com>
2018-06-15 10:35:05 +0200

    Try build steps

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,3 @@
-# Login to manawa
-oc login -u ${CLUSTER_USERNAME} -p ${CLUSTER_PASSWORD} ${CLUSTER_URL}
-
-# Create or select project in manawa
-{
-    echo "Select project ${PROJECT_NAME} or create if not exists."
-    oc project ${PROJECT_NAME}
-} || {
-    oc new-project ${PROJECT_NAME}
-}
-
-# Build and push docker image to manawa docker registry
-echo "Build and push on docker image on ${DOCKER_REGISTRY_URL}"
-docker login -u ${CLUSTER_USERNAME} -p $(oc whoami -t) ${DOCKER_REGISTRY_URL}
-docker build -t ${DOCKER_REGISTRY_URL}/${PROJECT_NAME}/hello-world:latest .
-docker push ${DOCKER_REGISTRY_URL}/${PROJECT_NAME}/hello-world:latest
-
 # Update project kubernetes objects.
 echo "Update kubernetes deployment"
 oc apply -f kubernetes/hello-world-deployment.yml
