969e9366915296750bb127dce03b91f9fde30b87
Matthieu REMY <remy.matthieu@gmail.com>
2018-06-15 10:05:04 +0200

    Add echo commands

--- deploy.sh
+++ deploy.sh
@@ -10,11 +10,13 @@ oc login -u ${CLUSTER_USERNAME} -p ${CLUSTER_PASSWORD} ${CLUSTER_URL}
 }
 
 # Build and push docker image to manawa docker registry
+echo "Build and push on docker image on ${DOCKER_REGISTRY_URL}"
 docker login -u ${CLUSTER_USERNAME} -p (oc whoami -t) ${DOCKER_REGISTRY_URL}
 docker build -t ${DOCKER_REGISTRY_URL}/${PROJECT_NAME}/hello-world:latest .
 docker push ${DOCKER_REGISTRY_URL}/${PROJECT_NAME}/hello-world:latest
 
 # Update project kubernetes objects.
+echo "Update kubernetes deployment"
 oc apply -f kubernetes/hello-world-deployment.yml
 oc apply -f kubernetes/hello-world-service.yml
 oc apply -f kubernetes/hello-world-route.yml
