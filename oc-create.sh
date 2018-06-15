8e7edab6a21ea5a570e517ae8bc8ffa78a23c54c
Matthieu REMY <remy.matthieu@gmail.com>
2018-06-15 14:23:59 +0200

    Fix oc path

--- oc-create.sh
+++ oc-create.sh
@@ -1,10 +1,10 @@
 # Login to manawa
-/bin/oc login -u ${CLUSTER_USERNAME} -p ${CLUSTER_PASSWORD} ${CLUSTER_URL}
+oc login -u ${CLUSTER_USERNAME} -p ${CLUSTER_PASSWORD} ${CLUSTER_URL}
 
 # Create or select project in manawa
 {
     echo "Select project ${PROJECT_NAME} or create if not exists."
-    /bin/oc project ${PROJECT_NAME}
+    oc project ${PROJECT_NAME}
 } || {
-    /bin/oc new-project ${PROJECT_NAME}
+    oc new-project ${PROJECT_NAME}
 }
