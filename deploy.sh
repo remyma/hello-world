3b078f6ae17c0eeeb86d667097acce79f615f6e3
Matthieu REMY <remy.matthieu@gmail.com>
2018-06-18 11:07:25 +0200

    Remove unused script.

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-# Update project kubernetes objects.
-echo "Update kubernetes deployment"
-oc apply -f kubernetes/hello-world-deployment.yml
-oc apply -f kubernetes/hello-world-service.yml
-oc apply -f kubernetes/hello-world-route.yml
