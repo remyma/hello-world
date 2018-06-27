6f8f86433e385332590eb5742e3196be925e6393
Matthieu REMY <remy.matthieu@gmail.com>
2018-06-27 14:15:05 +0200

    Test deploy

--- src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
+++ src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Welcome to the devweek marem !\n";
+        return "Welcome to the devweek  !\n";
     }
     
 }
